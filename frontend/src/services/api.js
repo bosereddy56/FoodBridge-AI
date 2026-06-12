efe5e80ba9cc66d6e84dec14f0ef5140b4117d23
MALLIDI BHUVANA CHANDRA BOSE REDDY <bosereddy56@gmail.com>
2026-06-12 15:39:53 +0530

    Updated production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: "https://foodbridge-ai-d603.onrender.com/api",
 });
 
 export default API;
